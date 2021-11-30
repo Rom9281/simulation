4a260ec5546e119b1ab961b76a9ab9c309ae3a75
Rom9281 <34047259+Rom9281@users.noreply.github.com>
2021-11-30 12:09:15 +0100

    Add2Last

--- src/heberges/Heberge.java
+++ src/heberges/Heberge.java
@@ -1,11 +1,12 @@
 package heberges;
 
 import tp.model.agents.Animal;
+import tp.model.agents.Decor;
 
 /**
  * Interface pour animaux hebergés
  * Elle est moins forte que l'interface hebergeur */
 public interface Heberge {
-	public abstract boolean peutHabiter(Animal animal);
-	public abstract boolean habiter(Animal animal); 
+	public abstract boolean peutHabiter(Animal animal, Decor decor);
+	public abstract boolean habiter(Animal animal,Decor decor); 
 }
