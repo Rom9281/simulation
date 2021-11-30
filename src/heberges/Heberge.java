2f2792734c06aa56a50196306779c44e3647b18d
Rom9281 <34047259+Rom9281@users.noreply.github.com>
2021-11-30 12:08:46 +0100

    Edit: Heberge,Abeille

--- src/heberges/Heberge.java
+++ src/heberges/Heberge.java
@@ -1,5 +1,11 @@
 package heberges;
 
-public interface Heberge {
+import tp.model.agents.Animal;
 
+/**
+ * Interface pour animaux hebergés
+ * Elle est moins forte que l'interface hebergeur */
+public interface Heberge {
+	public abstract boolean peutHabiter(Animal animal);
+	public abstract boolean habiter(Animal animal); 
 }
--- src/tp/model/agents/Abeille.java
+++ src/tp/model/agents/Abeille.java
@@ -1,24 +1,40 @@
 package tp.model.agents;
 
 import java.awt.Point;
+
+import heberges.Heberge;
 import hebergeurs.Hebergeur;
+import java.util.HashSet;
 
 
-public class Abeille extends Animal implements Hebergeur{
+
+public class Abeille extends Animal implements Hebergeur,Heberge{
+	/*
+	 * Variables
+	 * */
+	
 	private boolean parasite = false;
-
+	protected HashSet population; // création de la variable contenat la population
+	
+	/*
+	 * Constructeurs
+	 * */
+	
 	public Abeille(Sexe sexe, Point coord) {
-		super(sexe, new Point(coord));
-		// TODO Auto-generated constructor stub
+		super(sexe, new Point(coord)); // appel le constructeur d'animal
 	}
 
 	public Abeille(Sexe sexe) {
-		this(sexe, new Point(0,0));
+		this(sexe, new Point(0,0)); // A
 	}
 
 	public Abeille() {
 		this(Sexe.Male,new Point(0,0));
 	}
+	
+	/*
+	 * Methodes
+	 * */
 	
 	/** Verifie si l'animal est un varroa */
 	public boolean peutAcceuillir(Animal animal) {
@@ -29,19 +45,34 @@ public class Abeille extends Animal implements Hebergeur{
 			return false; // sinon non
 		}
 	}
+	
 	/** Permet l'acceuil du parasite */
 	public boolean acceuillir(Animal animal) {
 		if(this.peutAcceuillir(animal)){
+			
 			if(!(this.parasite)) { // Si l'animal est un varroa
+				
 				this.parasite = true; // le prendre
 				return true; // valider la demarche
+				
 			}
 			else {
 				return false; // signaler l'echec de la fonction
-			}		
+			}
+			
 		}
 		else {
 			return false;
 		}
-	}	
+	}
+	
+	/**
+	 * Utilisé pour specifier qui est hebergé*/
+	public boolean peutHabiter(Decor decor){
+		return true;
+	}
+	
+	public boolean habiter(Decor decor){
+		return true;
+	}
 }
